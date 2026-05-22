c6396dd080d291fd48345a01827b8f7be2797b83
David Atanda <ololadedavid15@gmail.com>
2026-05-22 09:29:49 +0100

    feat: animate

--- src/pages/ToluBirthday.js
+++ src/pages/ToluBirthday.js
@@ -13,31 +13,31 @@ const ToluBirthday = ()=>{
             <div className="container">                
                 <div className="row w-100 p-md-5 px-2 pt-4">
                     <div className="col-md-6 my-2">
-                        <p className="text-danger satisfy-regular fs-4">
+                        <p className="text-danger satisfy-regular fs-4" data-aos="fade-down">
                             Happy Birthday, Toluwanimi 🎉
                         </p>
-                        <img src={Tolu} className="img-fluid" />
+                        <img src={Tolu} className="img-fluid" data-aos="fade-right" />
                     </div>                    
                     <div className="col-md-6 my-2">
-                        <p className="text-white text-justify">
+                        <p className="text-white text-justify" data-aos="fade">
                             As you celebrate another year today, I wish you nothing but the very best life has to offer. Your birthday is always a special occasion I look forward to because it reminds me of how truly wonderful you are. It is also a time for me to reflect on your growth, not only as a professional, but also as a woman of faith and purpose. 
                         </p>
-                        <p className="text-white text-justify">
+                        <p className="text-white text-justify" data-aos="fade">
                             You are the kind of friend everyone needs in their corner; supportive, progressive, radiant, and genuinely kind. It has been beautiful watching you grow, evolve, and continue to make meaningful progress in every area of your life.
                         </p> 
-                        <p className="text-white text-justify">
+                        <p className="text-white text-justify" data-aos="fade">
                             Thank you for being you, 
                         </p>
-                        <p className="text-white text-justify">
+                        <p className="text-white text-justify" data-aos="fade">
                             Thank you for being the “Yemi” in my Adeyemi 
                         </p>
-                        <p className="text-white text-justify">
+                        <p className="text-white text-justify" data-aos="fade">
                             Thank you for being the best person to raise a Godly home with. 
                         </p>
-                        <p className="text-white text-justify">
+                        <p className="text-white text-justify" data-aos="fade">
                             I love you today, tomorrow and forever 
                         </p>
-                        <p className="text-white text-justify">
+                        <p className="text-white text-justify" data-aos="fade">
                             Teminikan Omo Baba Adejubee 
                         </p>   
                         <div className="d-flex justify-content-between">
